03e8c63fd4681295601fb2a2f96d9bc0cb063c42
Januka Perera <pererajanuka2000@gmail.com>
2026-02-17 08:56:15 +1100

    Added inspections for metadata in object detection file

--- inspect_mat.py
+++ inspect_mat.py
@@ -7,5 +7,16 @@ print(f"File {mat_path}")
 
 data = scipy.io.loadmat(str(mat_path))
 
-uv_coordinates = data["metadata"]["gcps"][0,0]["UVpicked"][0,0]
-print(uv_coordinates)
+meta = data["metadata"]
+print("metadata fields:", meta.dtype.names)
+
+geom = data["metadata"]["geom"][0,0]
+print("geom fields:", geom.dtype.names)
+
+gcps = data["metadata"]["gcps"][0,0]
+print("gcp fields:", gcps.dtype.names)
+
+lcp = data["metadata"]["geom"][0, 0]["lcp"][0, 0]                                                                              
+print("lcp fields:", lcp.dtype.names)                                                                                          
+print("NU:", lcp["NU"][0, 0].item())                                                                                           
+print("NV:", lcp["NV"][0, 0].item())
